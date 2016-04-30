bc1203c6faa0d57ca9ccc4e42b3dbc424107154b
oi7 <oi7@mit.edu>
2016-04-30 06:29:09 -0400

    Implemented Quick Actions from Home screen for 3 scopes

--- iScope/AppDelegate.swift
+++ iScope/AppDelegate.swift
@@ -11,36 +11,122 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    enum ShortcutIdentifier: String {
+        case Microscope
+        case Spheroscope
+        case Telescope
+        
+        // MARK: Initializers
+        
+        init?(fullType: String) {
+            guard let last = fullType.componentsSeparatedByString(".").last else { return nil }
+            
+            self.init(rawValue: last)
+        }
+        
+        // MARK: Properties
+        
+        var type: String {
+            return NSBundle.mainBundle().bundleIdentifier! + ".\(self.rawValue)"
+        }
+    }
+    
+    // MARK: Static Properties
+    
+    static let applicationShortcutUserInfoIconKey = "applicationShortcutUserInfoIconKey"
+    
+    // MARK: Properties
+    
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    
+    /// Saved shortcut item used as a result of an app launch, used later when app is activated.
+    var launchedShortcutItem: UIApplicationShortcutItem?
+    
+    func handleShortCutItem(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var handled = false
+        
+        // Verify that the provided `shortcutItem`'s `type` is one handled by the application.
+        guard ShortcutIdentifier(fullType: shortcutItem.type) != nil else { return false }
+        
+        guard let shortCutType = shortcutItem.type as String? else { return false }
+        
+        switch (shortCutType) {
+        case ShortcutIdentifier.Microscope.type:
+            handled = true
+            break
+        case ShortcutIdentifier.Spheroscope.type:
+            handled = true
+            break
+        case ShortcutIdentifier.Telescope.type:
+            handled = true
+            break
+        default:
+            break
+        }
+        
+        // Construct an alert using the details of the shortcut used to open the application.
+        let alertController = UIAlertController(title: "Shortcut Handled", message: "\"\(shortcutItem.localizedTitle)\"", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(okAction)
+        
+        // Display an alert indicating the shortcut selected from the home screen.
+        window!.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+        
+        return handled
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
+    
+    // MARK: Application Life Cycle
+    
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        guard let shortcut = launchedShortcutItem else { return }
+        
+        handleShortCutItem(shortcut)
+        
+        launchedShortcutItem = nil
     }
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Override point for customization after application launch.
+        var shouldPerformAdditionalDelegateHandling = true
+        
+        // If a shortcut was launched, display its information and take the appropriate action
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsShortcutItemKey] as? UIApplicationShortcutItem {
+            
+            launchedShortcutItem = shortcutItem
+            
+            // This will block "performActionForShortcutItem:completionHandler" from being called.
+            shouldPerformAdditionalDelegateHandling = false
+        }
+        
+        // Install dynamic shortcuts.
+        if let shortcutItems = application.shortcutItems where shortcutItems.isEmpty {
+            // Construct the items.
+            let shortcut1 = UIMutableApplicationShortcutItem(type: ShortcutIdentifier.Microscope.type, localizedTitle: "Microscope", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(templateImageName: "microscope"), userInfo: nil)
 
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+            
+            let shortcut2 = UIMutableApplicationShortcutItem(type: ShortcutIdentifier.Spheroscope.type, localizedTitle: "Spheroscope", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(templateImageName: "spheroscope"), userInfo: nil)
+
+            
+            let shortcut3 = UIMutableApplicationShortcutItem(type: ShortcutIdentifier.Telescope.type, localizedTitle: "Telescope", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(templateImageName: "telescope"), userInfo: nil)
+            
+            // Update the application providing the initial 'dynamic' shortcut items.
+            application.shortcutItems = [shortcut1, shortcut2, shortcut3]
+        }
+        
+        return shouldPerformAdditionalDelegateHandling
     }
-
-
+    
+    /*
+     Called when the user activates your application by selecting a shortcut on the home screen, except when
+     application(_:,willFinishLaunchingWithOptions:) or application(_:didFinishLaunchingWithOptions) returns `false`.
+     You should handle the shortcut in those callbacks and return `false` if possible. In that case, this
+     callback is used if your application is already launched in the background.
+     */
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: Bool -> Void) {
+        let handledShortCutItem = handleShortCutItem(shortcutItem)
+        
+        completionHandler(handledShortCutItem)
+    }
 }
 
