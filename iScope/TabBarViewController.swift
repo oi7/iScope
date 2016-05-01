4e9bc8a617d79b70fb3c79dd82f9a08d7a443be3
oi7 <oi7@mit.edu>
2016-05-01 14:30:20 -0400

    Customized UITabBar and TabBarItems

---
+++ iScope/TabBarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarViewController.swift
+//  iScope
+//
+//  Created by Poseidon Ho on 5/1/16.
+//  Copyright © 2016 oi7. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+        self.title = nil;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension UITabBar {
+    
+    override public func sizeThatFits(size: CGSize) -> CGSize {
+        super.sizeThatFits(size)
+        var sizeThatFits = super.sizeThatFits(size)
+        sizeThatFits.height = 60
+        return sizeThatFits
+    }
+}
