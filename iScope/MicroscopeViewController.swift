8c047bdb6284e1d91237c8952899ea38c5bb8df4
oi7 <oi7@mit.edu>
2016-04-30 21:01:15 -0400

    Implemented image tagging and buttons in MicroscopeViewController

--- iScope/MicroscopeViewController.swift
+++ iScope/MicroscopeViewController.swift
@@ -8,45 +8,99 @@
 
 import UIKit
 
-class MicroscopeViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MicroscopeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var picker = UIImagePickerController()
+    static let conceptName: String? = nil
+    static let conceptNamespace = "default"
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
     
+    private lazy var client : ClarifaiClient =
+        ClarifaiClient(appID: clarifaiClientID, appSecret: clarifaiClientSecret)
     
+    @IBAction func openCamera(sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .Camera
+        picker.allowsEditing = false
+        picker.delegate = self
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
 
+    @IBAction func shareButton(sender: UIButton) {
+        let image = generateImage()
+        
+        let activity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        activity.completionWithItemsHandler = { (activityType: String?, completed: Bool, returnedItems: [AnyObject]?, activityError: NSError?) -> Void in
+            if completed {
+                activity.dismissViewControllerAnimated(true, completion: nil)
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+        presentViewController(activity, animated: true, completion: nil)
+    }
+    
+    func generateImage() -> UIImage {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+    
+    private func recognizeImage(image: UIImage!) {
+        // Scale down the image. This step is optional. However, sending large images over the
+        // network is slow and does not significantly improve recognition performance.
+        let size = CGSizeMake(320, 320 * image.size.height / image.size.width)
+        UIGraphicsBeginImageContext(size)
+        image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // Encode as a JPEG.
+        let jpeg = UIImageJPEGRepresentation(scaledImage, 0.9)!
+        
+        // Send the JPEG to Clarifai for standard image tagging.
+        client.recognizeJpegs([jpeg]) {
+            (results: [ClarifaiResult]?, error: NSError?) in
+            if error != nil {
+                print("Error: \(error)\n")
+                self.textView.text = "Sorry, there was an error recognizing your image."
+            } else {
+                self.textView.text = "Tags:\n" + results![0].tags.joinWithSeparator(", ")
+            }
+//            self.tagButton.enabled = true
+        }
+    }
+
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            // The user picked an image. Send it Clarifai for recognition.
+            imageView.image = image
+            //            backgroundImageView.hidden = true
+            textView.text = "Recognizing..."
+            //            button.enabled = false
+            recognizeImage(image)
+        }
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    //MARK: Picker Delegates
-    func imagePickerController(
-        picker: UIImagePickerController,
-        didFinishPickingMediaWithInfo info: [String : AnyObject])
-    {
-        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        UIImageWriteToSavedPhotosAlbum(chosenImage, self,nil, nil)
-    }
-    
-    //What to do if the image picker cancels.
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true,
-                                      completion: nil)
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
